2d19893643564bbecec796db28c3e2da05bf4453
NauNhieuSua <haitran.7642@gmail.com>
2023-01-10 11:32:50 +0700

    11h30 ngày 10.01.2023

--- IF/IF_TH4/IF_TH4.js
+++ IF/IF_TH4/IF_TH4.js
@@ -37,3 +37,19 @@
 // }
 
 // let giatri = prompt(" Nhap gia tri")
+
+let giatri = + prompt("Nhap gia tri")
+
+
+switch (giatri=0 ? 0 : giatri > 0 ? 1 :-1) {
+    case 0 :
+        alert(0);
+        break;
+    case 1 :
+        alert(1);
+        break;
+    case -1 :
+        alert(-1)
+        break;
+}
+
